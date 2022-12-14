7f7f2a1061bc6bb8a992173d4e33b147bf3b580e
Jacob Kearschner <jacobkearschner@gmail.com>
2022-12-14 11:04:52 -0500

    begin tParser work

--- TableParser.py
+++ TableParser.py
@@ -1,3 +1,13 @@
-import lxml
+from io import StringIO
+from lxml import etree
 
+oasisFile = open("oasisDump.txt", "r");
 
+htmlTable : str = oasisFile.read();
+
+parser = etree.HTMLParser();
+tree = etree.parse(StringIO(htmlTable), parser);
+
+result = etree.tostring(tree.getroot(), pretty_print=True, method="html");
+
+print(result);
