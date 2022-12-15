b692d9183ce8c2618496c053871abfa969cfde89
kearschner <jacobkearschner@gmail.com>
2022-12-15 00:37:38 -0500

    fully parse oasis tables

--- CellHandle.py
+++ CellHandle.py
@@ -2,12 +2,12 @@ import datetime
 import functools
 import typing
 
-from SectionHandle import Day, Location, Section
+from SectionHandle import Day, Location, Range
 
 
 
 
-
+@functools.cache
 def convertDays(daysString : str) -> Day:
     days = Day(0);
 
@@ -16,15 +16,29 @@ def convertDays(daysString : str) -> Day:
 
     return days;
 
+@functools.lru_cache(maxsize=24)
+def hourMap(num : int, afternoon : bool) -> int:
+    if not afternoon:
+        return num % 12;
+    
+    if num == 12:
+        return 12;
+    
+    return num + 12;
 
-def convertTime(timeString : str) -> datetime.time:
+def convertTime(timeString : str) -> typing.Optional[datetime.time]:
+
+    if "N/A" in timeString:
+        return None;
+
+    if "TBA" in timeString:
+        return None;
 
     afternoon : bool = 'pm' in timeString;
     
-    hour = int(timeString[:2]);
-    hour = hour + 12 if afternoon else hour;
+    hour = hourMap(int(timeString[:2]), afternoon);
     
-    return datetime.time(hour, int(timeString[2:4]));
+    return datetime.time(hour, int(timeString[3:5]));
 
 
 def convertDate(timeString : str) -> datetime.date:
@@ -33,14 +47,25 @@ def convertDate(timeString : str) -> datetime.date:
 
 
 T = typing.TypeVar('T')
-def convertEach(operation : typing.Callable[[str], T], sep : str) -> typing.Callable[[str], tuple[T]]:
+def convertRange(operation : typing.Callable[[str], typing.Optional[T]], sep : str) -> typing.Callable[[str], typing.Optional[Range[T]]]:
 
-    return lambda input : tuple(map(operation, input.split(sep)));
+    def conversionFunc(input : str) -> typing.Optional[Range[T]]:
+        if sep not in input:
+            return None;
+        endPoints : list[str] = input.split(sep);
+        start = operation(endPoints[0]);
+        end = operation(endPoints[1]);
+
+        if start is None or end is None:
+            return None;
+        return Range(start, end);
+
+    return conversionFunc;
 
 
-convertTimeFrame = functools.cache(convertEach(convertTime, '-'));
+convertTimeFrame = functools.cache(convertRange(convertTime, '-'));
 
-convertTermDates = functools.cache(convertEach(convertDate, '-'));
+convertTermDates = functools.cache(convertRange(convertDate, '-'));
 
 
 def convertLocation(locString : str) -> typing.Optional[Location]:
@@ -52,3 +77,6 @@ def convertLocation(locString : str) -> typing.Optional[Location]:
 
 def convertSeats(capString : str, availString : str) -> int:
     return int(capString) - int(availString);
+
+def isPermitLocked(permString : str) -> bool:
+    return 'Y' == permString; 
--- TableParser.py
+++ TableParser.py
@@ -1,26 +1,84 @@
-from lxml import etree
+from lxml import etree, html
 import typing
 import SectionHandle
+import CellHandle
 
 def getPrimativeRow(rowElement : etree._Element) -> list[str]:
 	assert rowElement.tag == "tr"
 	return [etree.tostring(elem, encoding="unicode", method="text")[:-1] for elem in rowElement.iterchildren(tag="td")];
 
-def tBodyFromRoot(root : etree._Element) -> etree._Element:
-	return root[0][0][1];
 
-def yieldSections(rows : typing.Iterator[list[str]]) -> typing.Generator[SectionHandle.Section]:
-	pass
+def isNewSection(row : list[str]) -> bool:
+	return row[1] != '\xa0';
 
-oasisFile = open("oasisDump.txt", "r");
 
-htmlTable : str = oasisFile.read();
+def firstRowSection(row : list[str]) -> SectionHandle.Section:
+	sect = SectionHandle.Section(
+							row[1],
+							SectionHandle.Course(row[2], row[3]),
+							row[4],
+							row[5],
+							row[6],
+							SectionHandle.InstructionalMethod.fromString(row[7]),
+							CellHandle.isPermitLocked(row[8]),
+							int(row[12]),
+							int(row[13]));
+	sect.lectureDays.append(SectionHandle.LectureDay(
+											CellHandle.convertTermDates(row[9]),
+											CellHandle.convertDays(row[10]),
+											CellHandle.convertTimeFrame(row[11]),
+											row[16],
+											row[17],
+											CellHandle.convertLocation(row[18]),
+											row[19]));
+	
+	return sect;
 
-parser : etree.HTMLParser = etree.HTMLParser();
-root : etree._Element = etree.fromstring(htmlTable,  parser);
+def yieldSections(rows : typing.Iterator[list[str]]) -> typing.Iterator[SectionHandle.Section]:
+	
+	currSect = None;
 
-tableBody : etree._Element = tBodyFromRoot(root);
+	for row in rows:
 
-cellGenerator : typing.Iterator[list[str]] = ((getPrimativeRow(elem)) for elem in tableBody.iterchildren(tag="tr"));
-for cell in cellGenerator:
-	print(cell);
+		if len(row) != 20:
+			continue;
+		
+		if currSect is None:
+			currSect = firstRowSection(row);
+
+		if not isNewSection(row):
+			currSect.lectureDays.append(SectionHandle.LectureDay(
+						CellHandle.convertTermDates(row[9]),
+						CellHandle.convertDays(row[10]),
+						CellHandle.convertTimeFrame(row[11]),
+						row[16],
+						row[17],
+						CellHandle.convertLocation(row[18]),
+						row[19]));
+			continue;
+
+		if len(currSect.lectureDays) > 1:
+			yield currSect;
+			currSect = firstRowSection(row);
+		
+		
+	if (currSect is not None):
+		yield currSect;
+
+def sectionsFromTableDump(filePath : str) -> typing.Iterator[SectionHandle.Section]:
+
+	oasisFile = open(filePath, "r");
+
+	htmlTable : str = oasisFile.read();
+
+	parser : etree.HTMLParser = etree.HTMLParser();
+	root : etree._Element = html.fromstring(htmlTable, parser=parser);
+
+	tableBody : etree._Element = root[1];
+
+	return yieldSections(((getPrimativeRow(elem)) for elem in tableBody.iterchildren(tag="tr")));
+
+
+if __name__ == "__main__":
+	for sec in sectionsFromTableDump("oasisDump.txt"):
+		print(sec);
